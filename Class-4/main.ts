4dea2cdca652d4c2c4b18db57dfda7ffca72253b
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-06 19:39:06 -0700

    Add files via upload

---
+++ Class-4/main.ts
@@ -0,0 +1,59 @@
+// Logical Operators
+
+//let sN = "Ali" 
+//let iS = "Monday"
+//let iA = sN === 'Ali' && iS === 'Monday' //assign krdiya 
+//&& right or left side pr jo bhi ho sb true hona chahiye
+//let iA = true && true //isAllowed
+//console.log(iA);
+
+//let email = 'warsa@gmail.com'
+//let pass = 'fb12345'
+//let iA = email === 'warsa@gmail.com' && pass === 'fb12345'
+//console.log(iA)
+
+//let firstvalue = "A"
+//let secondvalue = "B"
+//let iA = firstvalue === "A"|| secondvalue === "B"
+//console.log(iA)
+
+//console.log(!true) //!, reverses true to false & vice versa
+
+//console.log(!(5 < 4))
+ 
+
+
+// {}, block piece, a code which is written inside of a curly bracket
+
+//If else statements //condition ki base pr chalta hai
+
+//let ispassed = true
+
+//if(ispassed){
+//console.log("bike milgai")
+//}
+//else {
+//console.log("rickshaw milega")
+//}
+
+//let vehicle = "by walk"
+
+//if(vehicle === 'car'){
+//console.log("car ki parking mei jao")
+//}
+
+//else if(vehicle === 'bike'){
+//console.log("bike ki parking mei jao")
+//}
+
+//else if(vehicle === 'truck'){
+//console.log("truck ki parking mei jao")
+//}
+
+//else if(vehicle === 'by walk'){
+//console.log("footpath pr jao")
+//}
+
+//else{
+//console.log("wapis jao")
+//}
